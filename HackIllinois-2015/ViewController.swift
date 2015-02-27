05a943c3be126d007d299f9ab990a4db5953604c
Alex Cordonnier <alexjcord@gmail.com>
2015-02-27 11:53:52 -0600

    Initial Commit

---
+++ HackIllinois-2015/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackIllinois-2015
+//
+//  Created by Alex Cordonnier on 2/27/15.
+//  Copyright (c) 2015 PointOfIgnition. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
