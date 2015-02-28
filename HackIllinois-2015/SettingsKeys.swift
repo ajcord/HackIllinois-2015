8d4ff769a53f12f0b078bfe06e08d8e246f33c8a
Brandon Groff <brandongroff@Brandon-Air-OS-X.local>
2015-02-28 09:40:18 -0600

    Add LatLon function for Bus

---
+++ HackIllinois-2015/SettingsKeys.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsKeys.swift
+//  HackIllinois-2015
+//
+//  Created by Brandon Groff on 2/28/15.
+//  Copyright (c) 2015 PointOfIgnition. All rights reserved.
+//
+
+import Foundation
+
+struct SettingsKeys{
+    static var homeLat = "40.099987"//Lat of PAR
+    static var homeLon = "-88.220301"//Lon of PAR
+    static var homeAddress = "Pennsylvania Avenue Residence, Urbana, Illinois"
+}
--- HackIllinois-2015/ViewController.swift
+++ HackIllinois-2015/ViewController.swift
@@ -8,15 +8,42 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    func getURLFromLatLon(origin_latitude:String, origin_longitude:String,dest_latitude:String,dest_longitude:String)->NSURL{
+        let version = "v2.2"
+        let format = "json"
+        let method = "GetPlannedTripByLatLon"
+        let key = "bc049e9f423e49e7946f069af686ca3d"
+        let route_id = "GREEN"
+        
+        
+        let url = NSURL(string: "https://developer.cumtd.com/api/\(version)/\(format)/\(method)?key=\(key)&origin_lat=\(origin_latitude)&origin_lon=\(origin_longitude)&destination_lat=\(dest_latitude)&destination_lon=\(dest_latitude)")
+        return url!
+    }
+    
     @IBAction func click(sender: AnyObject) {
         let version = "v2.2"
         let format = "json"
         let method = "GetRoute"
         let key = "bc049e9f423e49e7946f069af686ca3d"
         let route_id = "GREEN"
+        
+        var locManager = CLLocationManager()
+        locManager.requestWhenInUseAuthorization()
+        
+        var currentLocation:CLLocation = CLLocation()
+        
+        if(CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse || CLLocationManager.authorizationStatus() == CLAuthorizationStatus.Authorized){
+            //Definetly not copy and pasted from stackoverflow
+            currentLocation = locManager.location
+        }
+        
+        let curLat:String = "\(currentLocation.coordinate.longitude)"
+        let curLon:String = "\(currentLocation.coordinate.latitude)"
+        
         
         let url = NSURL(string: "https://developer.cumtd.com/api/\(version)/\(format)/\(method)?key=\(key)&route_id=\(route_id)")
         println(url)
@@ -40,8 +67,12 @@ class ViewController: UIViewController {
             println("response code: \(httpResponse.statusCode)")
             let jsonDict = NSJSONSerialization.JSONObjectWithData(urlData!, options: nil, error: &error) as NSDictionary
             println((jsonDict["routes"] as NSArray)[0]["route_id"])
+            let status = jsonDict["status"] as NSDictionary
+            if(status["code"] as Int != 200){
+                NSException(name: "Failed to get bus schedule",reason: status["msg"] as? String, userInfo: nil).raise()
+            }
         } else {
-            println("error")
+            NSException(name: "Failed to get bus schedule",reason: "Could not connect to Internet", userInfo: nil).raise()
 
         }
         
